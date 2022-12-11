5acd0fafca60f6415ad760cd013d0310540782e3
Sein Audie Sutantio <106362905+Kakar3737@users.noreply.github.com>
2022-12-11 15:22:09 +0700

    nampilin grafik

--- dinamis.js
+++ dinamis.js
@@ -1,4 +1,3 @@
-import {csv} from "https://cdn.skypack.dev/d3-fetch@3";
 function disp(){
     var data
     var i = 0
@@ -7,39 +6,19 @@ function disp(){
         if(ele[i].type="radio") {
             if(ele[i].checked){
                 if(ele[i].value=="book1"){
-                    // data <= read_csv("csv/book1.csv")
-                    console.log(1)
-                    csv("csv/book5.csv").then((data) => {
-                    console.log(data);
-                    });
+                    document.getElementById("grafik1").output = "grafik";
                 }
                 else if(ele[i].value=="book2"){
-                    // data <= read_csv("csv/book2.csv")
-                    console.log(2)
-                    csv("csv/book2.csv").then((data) => {
-                    console.log(data);
-                    });
+                    document.getElementById("grafik2").output = "grafik";
                 }
                 else if(ele[i].value=="book3"){
-                    // data <= read_csv("csv/book3.csv")
-                    console.log(3)
-                    csv("csv/book3.csv").then((data) => {
-                    console.log(data);
-                    });
+                    document.getElementById("grafik3").output = "grafik";
                 }
                 else if(ele[i].value=="book4"){
-                    // data <= read_csv("csv/book4.csv")
-                    console.log(4)
-                    csv("csv/book4.csv").then((data) => {
-                    console.log(data);
-                    });
+                    document.getElementById("grafik4").output = "grafik";
                 }
                 else{
-                    // data <= read_csv("csv/book5.csv")
-                    console.log(5)
-                    csv("csv/book5.csv").then((data) => {
-                    console.log(data);
-                    });
+                    document.getElementById("grafik5").output = "grafik";
                 }
                 
             }
