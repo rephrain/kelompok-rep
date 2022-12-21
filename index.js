c297bef08e82470e1f7e0d56702cab44ba5e938b
dittoryanda <6182001056@student.unpar.ac.id>
2022-12-22 05:38:13 +0700

    connect database, searching done, convert html to ejs

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import mysql from 'mysql';
 import bodyParser from "body-parser";
+import express from 'express';
 
 const PORT  = 8080;
 const app = express();
@@ -16,7 +17,41 @@ const pool = mysql.createPool({
     connectionLimit: 10,
 })
 
-app.get('/search')
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/search', (req, res) => {
+    const book = req.query.book
+    const nama_karakter = req.query.nama_karakter
+
+    console.log(book, nama_karakter)
+
+    const result = [{target:"helo", weight:3}]
+
+    if (book === undefined || nama_karakter === undefined) res.render('./search', {
+        result : result
+    })
+    else {
+        const result = pool.query(`SELECT target, weight FROM search_data WHERE source=? AND book=?`, [nama_karakter, book], (err, rows, fields) =>{
+            res.render('./search', {
+                result : rows
+            })
+        })
+    }   
+})
+
+app.post('/search', async (req, res) =>{
+    let book = req.body.book
+    let nama_karakter = req.body.nama_karakter
+
+    console.log(book, nama_karakter)
+    res.redirect('/search')
+})
+
+app.get('/undirected', (req, res) =>{
+    res.render('./undirected')
+})
 
 app.listen(PORT, ()=>{
     console.log("Ready");
